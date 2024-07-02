af22759fb14dacdd680cb076d9067d7f0c4701a0
mohanad <97195043+Balela200@users.noreply.github.com>
2024-07-03 01:32:15 +0300

    Player Menement
    Player IK animator and Camera player

--- Assets/Player/Script/PlayerSystem.cs
+++ Assets/Player/Script/PlayerSystem.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerSystem : MonoBehaviour
 {
     [Header("Player Movement")]
     public float speed;
+    public float rotaionSpeed;
+    public float gravity = 9.18f;
+
+    private Vector3 moveDirection = Vector3.zero;
 
     [Header("Player System")]
-    Rigidbody rb;
+    public Animator anim;
     CharacterController characterController;
-    // Start is called before the first frame update
+    public Rigidbody rb;
+
+    public LayerMask layerMask;
+
+    // IK
+    [Range(0f, 1f)]
+    public float DistanceToGround;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
         characterController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,8 +39,79 @@ public class PlayerSystem : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
 
-        Vector3 move = new Vector3(horizontal, 0, vertical);
-        characterController.Move(move * Time.deltaTime * speed);
+        if (characterController.isGrounded)
+        {
 
+
+            moveDirection = new Vector3(horizontal, 0, vertical);
+            moveDirection.Normalize();
+
+            // Rotate Player
+            if (moveDirection != Vector3.zero)
+            {
+                Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotaionSpeed * Time.deltaTime);
+            }
+
+            if (vertical >= 0.1f || vertical <= -0.1f)
+            {
+                anim.SetBool("Run", true);
+            }
+            else if (horizontal >= 0.1f || horizontal <= -0.1f)
+            {
+                anim.SetBool("Run", true);
+            }
+            else
+            {
+                anim.SetBool("Run", false);
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(moveDirection * Time.deltaTime * speed);
+    }
+
+    // IK foot
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if(anim)
+        {
+            anim.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1f);
+            anim.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1f);
+
+            anim.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1f);
+            anim.SetIKRotationWeight(AvatarIKGoal.RightFoot, 1f);
+
+            RaycastHit hit;
+            // Left Foot
+            Ray ray = new Ray(anim.GetIKPosition(AvatarIKGoal.LeftFoot) + Vector3.up, Vector3.down);
+            if(Physics.Raycast(ray, out hit, DistanceToGround + 1f, layerMask))
+            {
+                if(hit.transform.tag == "Walk")
+                {
+                    Vector3 footPosition = hit.point;
+
+                    footPosition.y += DistanceToGround;
+                    anim.SetIKPosition(AvatarIKGoal.LeftFoot, footPosition);
+                    anim.SetIKRotation(AvatarIKGoal.LeftFoot, Quaternion.LookRotation(transform.forward, hit.normal));
+                }
+            }
+
+            // Right Foot
+            ray = new Ray(anim.GetIKPosition(AvatarIKGoal.RightFoot) + Vector3.up, Vector3.down);
+            if (Physics.Raycast(ray, out hit, DistanceToGround + 1f, layerMask))
+            {
+                if (hit.transform.tag == "Walk")
+                {
+                    Vector3 footPosition = hit.point;
+
+                    footPosition.y += DistanceToGround;
+                    anim.SetIKPosition(AvatarIKGoal.RightFoot, footPosition);
+                    anim.SetIKRotation(AvatarIKGoal.RightFoot, Quaternion.LookRotation(transform.forward, hit.normal));
+                }
+            }
+        }
     }
 }
